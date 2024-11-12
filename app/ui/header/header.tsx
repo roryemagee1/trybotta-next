03ac09265185cde0609510f3d7af4b56d0d35e06
Rory Magee <roryemagee@gmail.com>
2024-11-12 11:00:41 -0800

    Implement active routes

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,23 +1,28 @@
 
+'use client';
+
 import styles from '@/app/home.module.css';
 import { museoModerno } from '@/app/fonts/fonts';
+import { usePathname } from 'next/navigation';
 
 import Link from 'next/link';
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <div className={styles.navbar}>
+    <div className={styles.headerbar}>
       <header>
         <h1 className={`${museoModerno.className} ${styles.trybotta}`}>trybotta</h1>
         <nav>
           <Link
             href="/"
           >
-            <h2 style={{ textDecoration: "underline"}} className={`${museoModerno.className} ${styles.navbar}`}>Blog</h2>
+            <h2 className={`${museoModerno.className} ${styles.navitem} ${pathname === "/" && styles.navitemactive}`}>Blog</h2>
           </Link>
           <Link
             href="/newsletter"
           >
-            <h2 className={`${museoModerno.className} ${styles.navbar}`}>Newsletter</h2>
+            <h2 className={`${museoModerno.className} ${styles.navitem} ${pathname === "/newsletter" && styles.navitemactive}` }>Newsletter</h2>
           </Link>
         </nav>
       </header>
